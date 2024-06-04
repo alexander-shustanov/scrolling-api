bf7b9573e52f4c23f37ec02011c80f37d71b3a83
alexander.shustanov <alexander.shustanov@gmail.com>
2024-06-04 16:54:22 +0400

    test mysql

--- src/main/java/ru/springpendal/scrolling/PersonController.java
+++ src/main/java/ru/springpendal/scrolling/PersonController.java
@@ -1,13 +1,20 @@
 package ru.springpendal.scrolling;
 
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.query.*;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.ScrollPosition;
 import org.springframework.data.domain.Window;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/person")
 @RequiredArgsConstructor
@@ -15,13 +22,15 @@ public class PersonController {
 
     private final PersonRepository personRepository;
 
+    private final EntityManager entityManager;
+
     @GetMapping("/by-lastname-offset/{lastname}")
     public Window<Person> findFirst10ByLastnameOffset(@PathVariable String lastname, @Nullable @RequestParam Long offset) {
         long start = System.currentTimeMillis();
 
         Window<Person> personWindow = personRepository.findFirst100ByLastnameOrderByFirstname(lastname, ScrollPosition.offset(offset != null ? offset : 0));
 
-        System.out.println("offsetTime: " + (System.currentTimeMillis() - start));
+        log.info("offsetTime: " + (System.currentTimeMillis() - start));
 
         return personWindow;
     }
@@ -38,42 +47,91 @@ public class PersonController {
         } while (personWindow.hasNext());
 
         long time = System.currentTimeMillis() - start;
-        System.out.println("offsetTime: " + time);
-//        offsetTime: 350119
+        log.info("offsetTime: " + time);
         return time;
     }
 
     @GetMapping("/traverse-lastname-keyset/{lastname}")
-    public long traverseKeySet(@PathVariable String lastname) {
+    public void traverseKeySet(@PathVariable String lastname) {
         long start = System.currentTimeMillis();
 
         ScrollPosition offset = ScrollPosition.keyset();
         Window<Person> personWindow;
         do {
             personWindow = personRepository.findFirst100ByLastnameOrderByFirstname(lastname, offset);
-            offset = personWindow.positionAt(personWindow.size() - 1);
+            if (personWindow.hasNext()) {
+                offset = personWindow.positionAt(personWindow.size() - 1);
+            }
         } while (personWindow.hasNext());
 
         long time = System.currentTimeMillis() - start;
-        System.out.println("keysetTime: " + time);
+        log.info("keysetTime: " + time);
 
-        return time;
+    }
+
+    @GetMapping("/traverse-lastname-keyset-manual/{lastname}")
+    public void traverseKeySetManual(@PathVariable String lastname) {
+        long start = System.currentTimeMillis();
+
+        Pageable pageable = Pageable.ofSize(100);
+
+        Person lastPerson = null;
+        List<Person> personWindow = personRepository.findPersonsByLastname(lastname, pageable);
+        while (!personWindow.isEmpty()) {
+            lastPerson = personWindow.get(personWindow.size() - 1);
+
+            personWindow = personRepository.findPersonsByLastnameAndFirstnameAndId(
+                lastname,
+                lastPerson.getFirstname(),
+                lastPerson.getId(),
+                pageable
+            );
+        }
+
+
+        long time = System.currentTimeMillis() - start;
+        log.info("keysetManualTime: " + time);
+    }
+
+    @GetMapping("/traverse-lastname-keyset-hibernate/{lastname}")
+    public void traverseKeySetHibernate(@PathVariable String lastname) {
+        long start = System.currentTimeMillis();
+
+        Session session = entityManager.unwrap(Session.class);
+
+        Query<Person> query = session.createQuery("select p from Person p where p.lastname=:lastname", Person.class)
+            .setParameter("lastname", lastname);
+
+        KeyedPage<Person> keyedPage = Page.first(100)
+            .keyedBy(List.of(
+                Order.asc(Person.class, "firstname"),
+                Order.asc(Person.class, "id")
+            ));
+
+        KeyedResultList<Person> keyedResultList;
+        do {
+            keyedResultList = query
+                .getKeyedResultList(keyedPage);
+
+            keyedPage = keyedResultList.getNextPage();
+        } while (!keyedResultList.isLastPage());
+
+        long time = System.currentTimeMillis() - start;
+        log.info("keysetHibernateTime: " + time);
     }
 
     @GetMapping("/by-lastname-keyset/{lastname}")
-    public Window<Person> findFirst10ByLastnameKeysetAfter(@PathVariable String lastname, @RequestParam @Nullable String firstname, @RequestParam @Nullable String id) {
+    public Window<Person> findFirst100ByLastnameKeysetAfter(@PathVariable String lastname, @RequestParam @Nullable String firstname, @RequestParam @Nullable String id) {
         long start = System.currentTimeMillis();
 
-        Window<Person> first10ByLastnameOrderByFirstname = personRepository.findFirst100ByLastnameOrderByFirstname(
+        Window<Person> personWindow = personRepository.findFirst100ByLastnameOrderByFirstname(
             lastname,
             firstname != null && id != null ?
                 ScrollPosition.of(Map.of("firstname", firstname, "id", id), ScrollPosition.Direction.FORWARD) :
                 ScrollPosition.keyset()
         );
 
-        System.out.println("keySetTime:" + (System.currentTimeMillis() - start));
-        return first10ByLastnameOrderByFirstname;
+        log.info("keySetTime:" + (System.currentTimeMillis() - start));
+        return personWindow;
     }
 }
-
-
--- src/main/java/ru/springpendal/scrolling/PersonRepository.java
+++ src/main/java/ru/springpendal/scrolling/PersonRepository.java
@@ -1,11 +1,29 @@
 package ru.springpendal.scrolling;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.ScrollPosition;
 import org.springframework.data.domain.Window;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
     Window<Person> findFirst100ByLastnameOrderByFirstname(String lastname, ScrollPosition scrollPosition);
+
+    @Query("SELECT p FROM Person p WHERE p.lastname = :lastname AND (p.firstname >= :firstname AND (p.firstname > :firstname OR p.id > :id)) ORDER BY p.firstname, p.id")
+    List<Person> findPersonsByLastnameAndFirstnameAndId(
+        @Param("lastname") String lastname,
+        @Param("firstname") String firstname,
+        @Param("id") Long id,
+        Pageable pageable);
+
+
+    @Query("SELECT p FROM Person p WHERE p.lastname = :lastname ORDER BY p.firstname, p.id")
+    List<Person> findPersonsByLastname(
+        @Param("lastname") String lastname,
+        Pageable pageable);
 
     Window<Person> findFirst5ByLastname(String lastname, ScrollPosition scrollPosition);
 }
